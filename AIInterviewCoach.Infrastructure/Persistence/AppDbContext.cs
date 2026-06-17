3748886de2f12c5b0b27afbdb7a4ef610a95d1f2
yaren0600 <begumozturk0600@gmail.com>
2026-06-18 02:24:06 +0300

    Katmanlı mimari oluşturuldu Katmanlar birbirleri ile bağlandı Veritabanı ile bağlantı sağlandı Users tablsou oluşturuldu

---
+++ AIInterviewCoach.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AIInterviewCoach.Domain.Entities;
+
+namespace AIInterviewCoach.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+}
+
+//Burada EF'ye şunu diyoruz 'Benim user isminde bir entity'm var' 
+// EF bunu biliyor ve bunu veritabanında Users tablosuna mapliyor.
+// Yani public DbSet<User> Users = CREATE TABLE Users demenin C# tarafındaki karşılığıdır.
--- AIInterviewCoach/Program.cs
+++ AIInterviewCoach/Program.cs
@@ -1,9 +1,19 @@
+using AIInterviewCoach.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
