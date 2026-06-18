c058755db1325cf28d1d7a180bd7d62b4549a455
yaren0600 <begumozturk0600@gmail.com>
2026-06-18 03:18:03 +0300

    Request api oluşturuldu Swagger üzerinden veritabanına veri ekleme işlemi gerçekleştirildi

---
+++ AIInterviewCoach/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using AIInterviewCoach.Application.DTOs;
+using AIInterviewCoach.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AIInterviewCoach.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterRequestDto request)
+    {
+        var result = await _authService.RegisterAsync(request);
+        
+        if(result == "Bu email adresi zaten kayıtlı.")
+        {
+            return BadRequest(result);
+        }
+
+        return Ok(result);
+    }
+}
--- AIInterviewCoach/Program.cs
+++ AIInterviewCoach/Program.cs
@@ -1,8 +1,12 @@
+﻿using AIInterviewCoach.Application.DTOs;
+using AIInterviewCoach.Application.Interfaces;
+using AIInterviewCoach.Domain.Entities;
 using AIInterviewCoach.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+using System.Security.Cryptography;
+using System.Text;
 using AIInterviewCoach.Application.Interfaces;
-using AIInterviewCoach.Infrastructure.Services;
+using AIInterviewCoach.Application.Services;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +19,10 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<IAuthService, AuthService>();
+// Bu ne demek? Biri IAuthService isterse ona AuthService ver.
+// Bu da Dependency Injection mantığıdır.
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
