3120a5965eeb3bc9f117d8d23f2db990d8807b01
yaren0600 <begumozturk0600@gmail.com>
2026-06-24 01:39:18 +0300

    .

--- AIInterviewCoach/Program.cs
+++ AIInterviewCoach/Program.cs
@@ -45,10 +45,8 @@ builder.Services.AddAuthentication(options =>
             ValidateAudience = true,
             ValidateLifetime = true,
             ValidateIssuerSigningKey = true,
-
             ValidIssuer = builder.Configuration["Jwt:Issuer"],
             ValidAudience = builder.Configuration["Jwt:Audience"],
-
             IssuerSigningKey = new SymmetricSecurityKey(
                 Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
         };
